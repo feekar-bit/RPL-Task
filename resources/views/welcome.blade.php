0c3db0fd87902a1b7b7dc2e6f195a52f5d074dd0
feekar-bit <mzulfikarabdullah18@gmail.com>
2026-07-30 12:01:37 +0700

    mengubah judul Landing Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <title>RPL Task Manager - Manajemen Tugas & Proyek RPL</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
